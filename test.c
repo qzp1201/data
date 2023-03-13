419fb094619017d27254d1319113be8d25ac33b1
qzp1201 <37210635+qzp1201@users.noreply.github.com>
2023-03-13 14:45:50 +0800

    Create test.c

---
+++ test.c
@@ -0,0 +1,43 @@
+#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/resource.h>
+#include <as-layout.h>
+#include <init.h>
+#include <kern_util.h>
+#include <os.h>
+
+int fibonaci(int i)
+{
+   if(i == 0)
+   {
+      return 0;
+   }
+   if(i == 1)
+   {
+      return 1;
+   }
+   return fibonaci(i-1) + fibonaci(i-2);
+}
+
+
+int main(){
+     char *mad;
+     int i;
+  for (i = 0; i < 10; i++)
+    {
+       printf("%d\t\n", fibonaci(i));
+    }
+     while(1){
+     mad = malloc(sizeof *mad + 1024);
+       sleep(1000);
+      }
+     return 0;
+}
+
